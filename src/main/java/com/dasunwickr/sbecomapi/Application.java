6453eeec8a9258f51701ae68c5f27f3289651702
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-07-15 20:10:16 +0530

    Initial commit

---
+++ src/main/java/com/dasunwickr/sbecomapi/Application.java
@@ -0,0 +1,13 @@
+package com.dasunwickr.sbecomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasunwickr/sbecomapi/SbEcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasunwickr.sbecomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

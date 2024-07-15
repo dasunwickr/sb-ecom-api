00e5127775896c2a54e7afbf07b9f761a8d8b7ea
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-07-15 20:24:45 +0530

    Config

---
+++ src/main/java/com/dasunwickr/sbecomapi/model/Product.java
@@ -0,0 +1,29 @@
+package com.dasunwickr.sbecomapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean isAvailable;
+    private int quantity;
+
+}
